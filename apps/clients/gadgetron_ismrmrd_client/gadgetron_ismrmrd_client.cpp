0a0bd44f010a6a91c1838bebea2347dc9e02d623
Kyle Greenslade <v-kylegre@microsoft.com>
2022-02-24 13:44:20 -0700

    Split out the GadgetronClientDependencyQueryReader

--- apps/clients/gadgetron_ismrmrd_client/gadgetron_ismrmrd_client.cpp
+++ apps/clients/gadgetron_ismrmrd_client/gadgetron_ismrmrd_client.cpp
@@ -39,6 +39,7 @@
 #include "GadgetronClientResponseReader.h"
 #include "GadgetronClientException.h"
 #include "GadgetronClientTextReader.h"
+#include "GadgetronClientDependencyQueryReader.h"
 
 #if defined GADGETRON_COMPRESSION_ZFP
 #include <zfp.h>
@@ -247,64 +248,7 @@ struct GadgetMessageScript
 
 
 
-class GadgetronClientDependencyQueryReader : public GadgetronClientMessageReader
-{
 
-public:
-  GadgetronClientDependencyQueryReader(std::string filename) : number_of_calls_(0) , filename_(filename)
-  {
-
-  }
-
-  virtual ~GadgetronClientDependencyQueryReader()
-  {
-
-  }
-
-  virtual void read(boost::asio::ip::tcp::socket* stream)
-  {
-    size_t recv_count = 0;
-
-    typedef unsigned long long size_t_type;
-    size_t_type len(0);
-    boost::asio::read(*stream, boost::asio::buffer(&len, sizeof(size_t_type)));
-
-    char* buf = NULL;
-    try {
-      buf = new char[len];
-      memset(buf, '\0', len);
-      memcpy(buf, &len, sizeof(size_t_type));
-    } catch (std::runtime_error &err) {
-      std::cerr << "DependencyQueryReader, failed to allocate buffer" << std::endl;
-      throw;
-    }
-
-
-    if (boost::asio::read(*stream, boost::asio::buffer(buf, len)) != len)
-    {
-      delete [] buf;
-      throw GadgetronClientException("Incorrect number of bytes read for dependency query");
-    }
-
-    std::ofstream outfile;
-    outfile.open (filename_.c_str(), std::ios::out|std::ios::binary);
-
-    if (outfile.good()) {
-      outfile.write(buf, len);
-      outfile.close();
-      number_of_calls_++;
-    } else {
-      delete[] buf;
-      throw GadgetronClientException("Unable to write dependency query to file");
-    }
-
-    delete[] buf;
-  }
-
-  protected:
-    size_t number_of_calls_;
-    std::string filename_;
-};
 
 
 class GadgetronClientImageMessageReader : public GadgetronClientMessageReader
